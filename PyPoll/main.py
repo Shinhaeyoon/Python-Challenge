3edba7a21214413abff8397bd36cb02d5481aacb
Shinhaeyoon <syoon21@hawk.iit.edu>
2023-01-15 00:14:19 -0600

    PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,40 +1,47 @@
+# import modules
 import os
 import csv
 
+# set path of csv file
 csvpath = os.path.join('Resources', 'election_data.csv')
 
+# counter for total number of votes
 total_votes = 0
 
+# make the lists of candidates, number of votes, percentage of votes
 candidates = []
 num_votes = []
 percent_votes = []
 
-
+# open csv file
 with open(csvpath) as csvfile:
     cr = csv.reader(csvfile, delimiter=',')
     csv_header = next(cr)
-    
+
+    # add name of candidates in the list of candidates, add one more vote to number of votes   
     for row in cr:
         total_votes += 1
         if row[2] not in candidates:
             candidates.append(row[2])
             index = candidates.index(row[2])
             num_votes.append(1)
-        
+
+        # if the candidate is already in the list, skip adding in the list of candidates, only add one more vote to number of votes        
         else:
             index = candidates.index(row[2])
             num_votes[index] += 1
 
+    # calculate the percentage of votes and add to the list
     for votes in num_votes:
         percentage = (votes/total_votes) * 100
         percent_votes.append(percentage)
 
-
+    # find out winner by getting the maximum value from the list
     winner = max(num_votes)
     winner_index = num_votes.index(winner)
     winning_candidate = candidates[winner_index]
 
-
+# print the output
 print(f"Election Results")
 print("----------------------------")
 print(f"Total Votes: {total_votes}")
@@ -47,7 +54,7 @@ print("----------------------------")
 
 
 
-
+# export output as text file
 output = os.path.join('analysis', 'PyPoll.txt')
 PP_output = open(output, "w")
 
